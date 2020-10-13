194ccc033778742c6fef46cdcfde3da943e67b0f
izaiah365 <izaiah.rodriguez@patelhs.org>
2020-10-13 18:09:24 +0000

    i changed the wording of my code

--- Main.java
+++ Main.java
@@ -1,8 +1,10 @@
+
 import java.util.Scanner;
- public class Main {
+
+public class Main {
   public static void main(String[] args) {
-    Scanner scan = new Scanner(System.in);  
-    System.out.println("Hello! welcome to my site whats your name?");
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Hello! welcome to tech support whats your name?");
     String name = scan.next();
 
     System.out.println("What is your age?");
@@ -12,20 +14,12 @@ import java.util.Scanner;
     String today = scan.next();
     today = today + scan.nextLine();
 
-    System.out.println("Thank you " + name + " you are " + age + " years old" );
+    System.out.println("Thank you " + name + " you are " + age + " years old");
     System.out.println("and your problem today is " + today);
 
-    System.out.print("we will get with you as soon as possible");
-
-    
+    System.out.print("An IT Tech will be with you as soon as possible");
 
 
-
-
-
-
-    
-    
 
   }
 }
