986b0a21ecf35cc545d0f922e7d8368d69e10dac
izaiah365 <izaiah.rodriguez@patelhs.org>
2020-10-13 17:51:19 +0000

    I used a tech support type and it gathers your information and gets with an it tech person to help you

--- Main.java
+++ Main.java
@@ -1,5 +1,31 @@
-class Main {
+import java.util.Scanner;
+ public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    Scanner scan = new Scanner(System.in);  
+    System.out.println("Hello! welcome to my site whats your name?");
+    String name = scan.next();
+
+    System.out.println("What is your age?");
+    int age = scan.nextInt();
+
+    System.out.println("Why are you needing help today?");
+    String today = scan.next();
+    today = today + scan.nextLine();
+
+    System.out.println("Thank you " + name + " you are " + age + " years old" );
+    System.out.println("and your problem today is " + today);
+
+    System.out.print("we will get with you as soon as possible");
+
+    
+
+
+
+
+
+
+    
+    
+
   }
 }
