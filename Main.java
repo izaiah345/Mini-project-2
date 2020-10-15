b3101a387fda45c60a5efc2071fe4d129ba91a8f
izaiah365 <izaiah.rodriguez@patelhs.org>
2020-10-15 16:13:24 +0000

    i put a with and else statement and put a loop

--- Main.java
+++ Main.java
@@ -10,14 +10,35 @@ public class Main {
     System.out.println("What is your age?");
     int age = scan.nextInt();
 
-    System.out.println("Why are you needing help today?");
-    String today = scan.next();
-    today = today + scan.nextLine();
+    System.out.println("Why are you needing help today? 1-computer power on 2-computer frezze");
+    int issue = scan.nextInt();
+    
+    issue=1;
+    issue=2;
+    
+    System.out.println( issue == issue );
+     issue = 1;
 
+      System.out.println( issue &= issue );
+      issue = 2;
+
+      System.out.println( issue != issue );
     System.out.println("Thank you " + name + " you are " + age + " years old");
-    System.out.println("and your problem today is " + today);
+    System.out.println("and your problem today is " + issue);
 
     System.out.print("An IT Tech will be with you as soon as possible");
+
+
+    System.out.println(" can you rate our serverices");
+    
+    int count = 1;
+    while (count <= 10)
+    {
+    System.out.println(count);
+    count++;
+    }
+ 
+
 
 
 
